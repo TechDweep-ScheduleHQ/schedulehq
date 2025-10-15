ee5a2826044375a43248f50d4b82145c1c43ea4c
akash-techdweep <akash@vrautomations.io>
2025-10-15 22:14:48 +0530

    client-booking page design and fuctionality completed

--- backend/src/Utilis/helper.ts
+++ backend/src/Utilis/helper.ts
@@ -5,13 +5,9 @@ export const getUsernameCacheKey = (username : string) => `username:${username}`
 export const getUserEmailCacheKey = (email : string) => `email:${email}`;
 export const getVerifiedCacheKey = (verified : boolean) => `verified:${verified}`;
 export const getUserOnboardingCacheKey = (userId : number) => `user:onboarding:${userId}`;
-<<<<<<< HEAD
-=======
 export const getUserEventCacheKey = (userId : number) => `event:userId:${userId}`;
 
->>>>>>> redis
 
 export const TIMEZONE_CACHE_KEY = 'timezones';
-
 
 
